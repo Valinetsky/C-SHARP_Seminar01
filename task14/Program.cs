445474a5b8bb007ae5eec644306a9fbb895a1bfa
Valinetsky <highrat@gmail.com>
2022-12-04 20:04:11 +0300

    Add task14 Square

---
+++ task14/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход 
+// два числа и проверяет, является ли одно число 
+// квадратом другого.
+Console.Write("Input first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 * num1 == num2)
+{
+	System.Console.WriteLine($"{num2} square of {num1}");
+}
+else
+{
+	System.Console.WriteLine("Not!");
+}
