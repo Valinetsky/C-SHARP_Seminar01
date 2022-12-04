b799fac4abe76026c892a2856542a569f4a7edf1
Valinetsky <highrat@gmail.com>
2022-12-04 14:32:36 +0300

    Add task11 removing central digit

---
+++ task11/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа
+Console.WriteLine("Hello, World!");
+
+// Напишите программу, которая выводит случайное число из отрезка [10, 99]
+// и показывает наибольшую цифру числа.
+
+int number = new Random().Next(100, 1000);
+int num1 = number / 100;
+int num2 = number % 10;
+int result = num1 * 10 + num2;
+Console.WriteLine("Random number");
+Console.WriteLine(number);
+
+Console.WriteLine(result);
