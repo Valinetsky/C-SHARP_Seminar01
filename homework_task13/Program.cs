bcd4ddbbe34e3ee0275c4fd177ee14c23d10f49a
Valinetsky <highrat@gmail.com>
2022-12-05 09:10:20 +0300

    Add Homework task13. Third digit

---
+++ homework_task13/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет.
+
+// 645-> 5
+
+// 78->третьей цифры нет
+
+// 32679 -> 6
+Console.Write("Input number: ");
+string strNumber = Console.ReadLine();
+int number = Convert.ToInt32(strNumber);
+
+// From string
+if (strNumber.Length <= 2)
+{
+	Console.WriteLine("No third digit");
+}
+else
+{
+	Console.WriteLine($"From string. Third digit: {strNumber[2]}");
+}
+
+// From number
+if (number < 100)
+{
+	Console.WriteLine("No third digit");
+}
+else
+{
+	int digits = 1 + Convert.ToInt32(Math.Truncate(Math.Log10(number)));
+	int result = number / Convert.ToInt32(Math.Pow(10, digits - 3)) % 10;
+	Console.WriteLine($"From number. Third digit: {result}");
+}
+
