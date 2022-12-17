4b94f9997bb8cc458e82afa3412a6ec1478540b2
Valinetsky <highrat@gmail.com>
2022-12-17 18:03:14 +0300

    Added folder homework task 36 + 38. Checking left and right position

---
+++ homework_task36/Program.cs
@@ -0,0 +1,98 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6]-> 0
+
+int size = inputNumberPrompt("Введите размер массива: ");
+
+if (size < 0)
+{
+	System.Console.WriteLine("В качестве размера будет использовано абсолютное значение введенного числа");
+	size = Math.Abs(size);
+}
+
+int[] myArray = new int[size];
+
+int leftRange;
+int rightRange;
+
+leftRange = inputNumberPrompt("Введите левую границу чисел в массиве: ");
+
+rightRange = inputNumberPrompt("Введите правую границу чисел в массиве: ");
+
+if (leftRange > rightRange)
+{
+	int exchange = leftRange;
+	leftRange = rightRange;
+	rightRange = exchange;
+}
+
+// System.Console.WriteLine("Левая граница = 100, правая = 999");
+
+// leftRange = 100;
+// rightRange = 999;
+
+arrayFill(myArray, leftRange, rightRange);
+// System.Console.WriteLine("88888888888888888888888888888888888");
+// Console.WriteLine("Массив: [ " + string.Join(" | ", myArray) + " ]");
+
+System.Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях: {sumInOddPosition(myArray)}");
+
+// -------------- SUM in array ------------
+int sumInOddPosition(int[] arr)
+{
+	int sum = 0;
+	for (int i = 0; i < arr.Length; i++)
+	{
+		if (i % 2 == 1)
+		{
+			System.Console.Write(arr[i] + " по индексу ");
+			System.Console.WriteLine(i);
+			sum = sum + arr[i];
+			System.Console.Write("Текущая сумма: ");
+			System.Console.WriteLine(sum);
+		}
+	}
+	return sum;
+}
+
+// ------------------- fill ARRAY
+int[] arrayFill(int[] arr, int left, int right)
+{
+	for (int i = 0; i < arr.Length; i++)
+	{
+		arr[i] = GetRandomFrom(left, right);
+	}
+	System.Console.WriteLine("=====================");
+	Console.WriteLine("Массив: [ " + string.Join(" | ", arr) + " ]");
+
+	return arr;
+}
+
+// --------------------- RANDOM NUMBER from - to -------------------
+int GetRandomFrom(int bottom, int top)
+{
+	Random rnd = new Random();
+	int result = rnd.Next(bottom, top + 1);
+	return result;
+}
+
+// ------------------------ safe input number
+int inputNumberPrompt(string str)
+{
+	int number;
+	string text;
+
+	while (true)
+	{
+		Console.Write($"{str} ");
+		text = Console.ReadLine();
+		if (int.TryParse(text, out number))
+		{
+			break;
+		}
+		Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
+	}
+	return number;
+}
---
+++ homework_task38/Program.cs
@@ -0,0 +1,112 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+int size = inputNumberPrompt("Введите размер массива: ");
+
+if (size < 0)
+{
+	System.Console.WriteLine("В качестве размера будет использовано абсолютное значение введенного числа");
+	size = Math.Abs(size);
+}
+
+double[] myArray = new double[size];
+
+int leftRange;
+int rightRange;
+
+leftRange = inputNumberPrompt("Введите левую границу чисел в массиве: ");
+
+rightRange = inputNumberPrompt("Введите правую границу чисел в массиве: ");
+
+int leftRangeNormalize = numberMin(leftRange, rightRange);
+
+int rightRangeNormalize = numberMax(leftRange, rightRange);
+
+leftRange = leftRangeNormalize;
+rightRange = rightRangeNormalize;
+
+System.Console.WriteLine("leftRange");
+System.Console.WriteLine(leftRange);
+System.Console.WriteLine("rightRange");
+System.Console.WriteLine(rightRange);
+
+
+arrayFill(myArray, leftRange, rightRange);
+// System.Console.WriteLine("88888888888888888888888888888888888");
+// Console.WriteLine("Массив: [ " + string.Join(" | ", myArray) + " ]");
+
+// System.Console.WriteLine($"Количество четных чисел в массиве: {sumInArray(myArray)}");
+
+// -------------- SUM in array ------------
+// int sumInArray(int[] arr)
+// {
+// 	int sum = 0;
+// 	for (int i = 0; i < arr.Length; i++)
+// 	{
+// 		if (arr[i] % 2 == 0)
+// 		{
+// 			System.Console.WriteLine(arr[i]);
+// 			sum++;
+// 		}
+// 	}
+// 	return sum;
+// }
+
+// ------------------- fill ARRAY
+double[] arrayFill(double[] arr, int left, int right)
+{
+	for (int i = 0; i < arr.Length; i++)
+	{
+		arr[i] = GetRandomFrom(left, right);
+	}
+	System.Console.WriteLine("=====================");
+	Console.WriteLine("Массив: [ " + string.Join(" | ", arr) + " ]");
+
+	return arr;
+}
+
+// ------------Two function to normalize left and right range
+int numberMin(int leftRange, int rightRange)
+{
+	return Math.Min(leftRange, rightRange);
+}
+
+int numberMax(int leftRange, int rightRange)
+{
+	return Math.Max(leftRange, rightRange);
+}
+
+// --------------------- RANDOM NUMBER from - to -------------------
+double GetRandomFrom(int bottom, int top)
+{
+	Random rnd = new Random();
+	// Random rndDouble = rnd.NextDouble();
+	double result = rnd.Next(bottom, top) + rnd.NextDouble();
+	// К целому случайному числу из диапазона добавляем случайное число
+	// из математического диапазона [0, 1) - единица не входит.
+	// Получим вещественное число не превышающее общий диапазон.
+	// Как-то непросто, но пока - так.
+	System.Console.WriteLine("=============================");
+	System.Console.WriteLine(result);
+	return result;
+}
+
+// ------------------------ safe input number
+int inputNumberPrompt(string str)
+{
+	int number;
+	string text;
+
+	while (true)
+	{
+		Console.Write($"{str} ");
+		text = Console.ReadLine();
+		if (int.TryParse(text, out number))
+		{
+			break;
+		}
+		Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
+	}
+	return number;
+}
